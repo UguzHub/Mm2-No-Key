2dc316a61ed78c04d4df45f3cb5f34146148304e
UguzHub <oguzyanhesap45@gmail.com>
2026-08-08 18:45:14 +0300

    Implement script loading for main menu and spawner
    Added two task spawns for loading scripts: one for the main menu and another for the visual MM2 spawner.

--- script.lua
+++ script.lua
@@ -1,1 +1,13 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/bMQ3he6vRO/0"))()
+-- 1. Main Menu
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://cdn.sourceb.in/bins/bMQ3he6vRO/0"))()
+    end)
+end)
+
+-- 2. Visual MM2 Spawner
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://codeberg.org/elysiummm/whatthehelly/raw/branch/main/visual/mm2_spawner"))()
+    end)
+end)
